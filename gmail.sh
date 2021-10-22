cf67ffe7c442bac906dd4b84a1020b301a5cbd3a
Alexander Tashi <tashimotor@gmail.com>
2021-10-22 16:14:50 +0300

    fix gmail links

--- gmail.sh
+++ gmail.sh
@@ -5,10 +5,11 @@ nativefier \
 --name "Gmail" \
 --width "800px" \
 --height "600px" \
---user-agent "safari" \
---inject "gmail.js" \
+--user-agent "edge" \
+--inject "./gmail.js" \
 --browserwindow-options '{ "webPreferences": { "spellcheck": true } }' \
 --verbose \
 --single-instance \
 --disable-gpu \
---ignore-gpu-blacklist
+--ignore-gpu-blacklist \
+--internal-urls "mail.google.*?" \
